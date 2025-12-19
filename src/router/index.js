22c14290c3f3d62bfb8559ee4543e4771bba05e1
Léa RYSSEN <lea.ryssen@etu.umontpallier.fr>
2025-12-19 17:25:09 +0100

    modif pour publication

--- src/router/index.js
+++ src/router/index.js
@@ -7,15 +7,18 @@ import Project from '../views/Project.vue'
 import Contact from '../views/Contact.vue'
 
 const routes = [
-  { path: '/', component: Home },
-  { path: '/cv', component: CV },
-  { path: '/projets', component: Projects },
-  { path: '/projets/:id', component: Project },
-  { path: '/contact', component: Contact }
+  { path: '/', name: 'Home', component: Home },
+  { path: '/cv', name: 'CV', component: CV },
+  { path: '/projets', name: 'Projects', component: Projects },
+  { path: '/projets/:id', name: 'Project', component: Project },
+  { path: '/contact', name: 'Contact', component: Contact },
+  // Redirection automatique si une page n'est pas trouvée (utile pour GitHub Pages)
+  { path: '/:pathMatch(.*)*', redirect: '/' }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  // On ajoute import.meta.env.BASE_URL pour que le routeur s'adapte à l'URL de GitHub
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
